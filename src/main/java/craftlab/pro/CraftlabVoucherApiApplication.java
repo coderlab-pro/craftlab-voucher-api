af45c53abe408f57de4b15c88c661f150e521279
coderlab-tech-mg <coderlab.tech@proton.me>
2024-07-12 09:03:43 +0300

    chore: init project

---
+++ src/main/java/craftlab/pro/CraftlabVoucherApiApplication.java
@@ -0,0 +1,13 @@
+package craftlab.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftlabVoucherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftlabVoucherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/craftlab/pro/CraftlabVoucherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package craftlab.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftlabVoucherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
